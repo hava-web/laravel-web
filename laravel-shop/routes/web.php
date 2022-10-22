189f02837b13801f0998859dc500322246c7ca0c
unknown <nguyenngocha18062002@gmail.com>
2022-10-22 11:26:38 +0700

    first commit

---
+++ laravel-shop/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->group(function (){
+    Route::get('/dashboard',[App\Http\Controllers\Admin\DashboardController::class,'index']);
+
+});
+
+Route::group(['middleware' => ['auth']], function() {
+    
+    Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+ });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
