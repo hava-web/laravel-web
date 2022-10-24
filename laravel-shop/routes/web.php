9d53188c457737bc7b3105ec38aff7b605736928
unknown <nguyenngocha18062002@gmail.com>
2022-10-24 21:22:19 +0700

    add admin interface

--- laravel-shop/routes/web.php
+++ laravel-shop/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -13,7 +14,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('admin')->group(function (){
-    Route::get('/dashboard',[App\Http\Controllers\Admin\DashboardController::class,'index']);
+    Route::get('dashboard',[App\Http\Controllers\Admin\DashboardController::class,'index']);
 
 });
 
